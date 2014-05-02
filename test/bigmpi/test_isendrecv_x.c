ec3709610adc23939c360dd325002d8518fa8785
Jeff Hammond <jeff.science@gmail.com>
2014-05-02 14:55:22 -0500

    add nonblocking send-recv test

--- test/bigmpi/test_isendrecv_x.c
+++ test/bigmpi/test_isendrecv_x.c
@@ -13,6 +13,13 @@ const MPI_Count test_int_max = BIGMPI_MAX_INT;
 const MPI_Count test_int_max = INT_MAX;
 #endif
 
+int MPIX_Isend_x(const void *buf, MPI_Count count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm, MPI_Request *request);
+int MPIX_Irecv_x(void *buf, MPI_Count count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Request *request);
+
+int MPIX_Sendrecv_x(const void *sendbuf, MPI_Count sendcount, MPI_Datatype sendtype, int dest, int sendtag,
+                    void *recvbuf, MPI_Count recvcount, MPI_Datatype recvtype, int source, int recvtag, MPI_Comm comm, MPI_Status *status);
+int MPIX_Sendrecv_replace_x(void *buf, MPI_Count count, MPI_Datatype datatype, int dest, int sendtag, int source, int recvtag, MPI_Comm comm, MPI_Status *status);
+
 /* Yes, it is technically unsafe to cast MPI_Count to MPI_Aint or size_t without checking,
  * given that MPI_Count might be 128b and MPI_Aint and size_t might be 64b, but BigMPI
  * does not aspire to support communication of more than 8 EiB messages at a time.
@@ -43,13 +50,21 @@ int main(int argc, char * argv[])
 
     for (int r = 1; r < size; r++) {
 
+        MPI_Request req;
+
         /* pairwise communication */
         if (rank==r) {
-            MPIX_Send_x(buf, n, MPI_CHAR, 0 /* dst */, r /* tag */, MPI_COMM_WORLD);
+            MPIX_Isend_x(buf, n, MPI_CHAR, 0 /* dst */, r /* tag */, MPI_COMM_WORLD, &req);
         }
         else if (rank==0) {
-            MPIX_Recv_x(buf, n, MPI_CHAR, r /* src */, r /* tag */, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPIX_Irecv_x(buf, n, MPI_CHAR, r /* src */, r /* tag */, MPI_COMM_WORLD, &req);
+        }
 
+        if (rank == 0 || rank==r) {
+            MPI_Wait(&req, MPI_STATUS_IGNORE);
+        }
+
+        if (rank==0) {
             /* correctness verification */
             size_t errors = 0;
             for (size_t i = 0; i < (size_t)n; i++) {
