dd23a852cd8fc7be2a51585f28ffab28189ee348
Jeff Hammond <jeff.science@gmail.com>
2014-05-02 11:41:30 -0500

    add a vector version of newtype constructor to amortize overead

--- src/type_contiguous_x.c
+++ src/type_contiguous_x.c
@@ -74,3 +74,72 @@ int MPIX_Type_contiguous_x(MPI_Count count, MPI_Datatype oldtype, MPI_Datatype *
     return MPI_SUCCESS;
 }
 
+#if 0
+/*
+ * Synopsis
+ *
+ * int MPIX_Type_contiguousv_x(int ncount,
+ *                             MPI_Count counts[],
+ *                             MPI_Datatype   oldtypes[],
+ *                             MPI_Datatype * newtypes[])
+ *
+ *  Input Parameters
+ *
+ *   ncount            length of arrays of counts and types
+ *   counts            replication count (nonnegative integer)
+ *   oldtypes          old datatype (handle array)
+ *
+ * Output Parameter
+ *
+ *   newtypes           new datatype (handle array)
+ *
+ */
+int MPIX_Type_contiguousv_x(int ncount, MPI_Count counts[], MPI_Datatype oldtypes[], MPI_Datatype * newtypes[])
+{
+    int rc = MPI_SUCCESS;
+
+    TODO: Implement this function
+
+    MPI_Count c = count/BIGMPI_MAX;
+    MPI_Count r = count%BIGMPI_MAX;
+
+    MPI_Datatype chunk;
+    rc = MPI_Type_contiguous(BIGMPI_MAX, oldtype, &chunk);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    MPI_Datatype chunks;
+    rc = MPI_Type_contiguous(c, chunk, &chunks);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    MPI_Datatype remainder;
+    rc = MPI_Type_contiguous(r, oldtype, &remainder);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    int typesize;
+    rc = MPI_Type_size(oldtype, &typesize);
+
+    MPI_Aint remdisp                   = (MPI_Aint)c*BIGMPI_MAX*typesize; /* must explicit-cast to avoid overflow */
+    int array_of_blocklengths[2]       = {1,1};
+    MPI_Aint array_of_displacements[2] = {0,remdisp};
+    MPI_Datatype array_of_types[2]     = {chunks,remainder};
+    rc = MPI_Type_create_struct(2, array_of_blocklengths, array_of_displacements, array_of_types, newtype);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    /* For MPIX_Type_contiguous_x to behave like it should, we do not commit the datatype internally. */
+    /*
+    rc = MPI_Type_commit(newtype);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+    */
+
+    rc = MPI_Type_free(&chunk);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    rc = MPI_Type_free(&chunks);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    rc = MPI_Type_free(&remainder);
+    MPI_ASSERT(rc==MPI_SUCCESS);
+
+    return MPI_SUCCESS;
+}
+#endif
