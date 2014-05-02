c9d2eb8e80ab05610a6486ba5b3fa2e9b56b9a9b
Jeff Hammond <jeff.science@gmail.com>
2014-05-02 13:27:55 -0500

    fix duplicate symbol issue with static

--- src/bigmpi_impl.h
+++ src/bigmpi_impl.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <limits.h>
 
 #include <mpi.h>
@@ -13,10 +12,10 @@
 #include "likely.h"
 
 #ifdef BIGMPI_MAX_INT
-const MPI_Count bigmpi_int_max = BIGMPI_MAX_INT;
+static const MPI_Count bigmpi_int_max = BIGMPI_MAX_INT;
 #else
 #include <limits.h>
-const MPI_Count bigmpi_int_max = INT_MAX;
+static const MPI_Count bigmpi_int_max = INT_MAX;
 #endif
 
 #endif // BIGMPI_IMPL_H
