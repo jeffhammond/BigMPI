e0becd926c1f5a71736495fd9f8b743547396cb1
Jeff Hammond <jeff.science@gmail.com>
2014-05-06 14:43:05 -0500

    more debugging of the test failures

--- test/bigmpi/test_contig_x.c
+++ test/bigmpi/test_contig_x.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
 #include <mpi.h>
 #include "bigmpi.h"
+
+
+static void print_typename(MPI_Datatype type)
+{
+    char * name;
+    if      (type==MPI_CHAR              ) name="MPI_CHAR              ";
+    else if (type==MPI_SIGNED_CHAR       ) name="MPI_SIGNED_CHAR       ";
+    else if (type==MPI_UNSIGNED_CHAR     ) name="MPI_UNSIGNED_CHAR     ";
+    else if (type==MPI_BYTE              ) name="MPI_BYTE              ";
+    else if (type==MPI_WCHAR             ) name="MPI_WCHAR             ";
+    else if (type==MPI_SHORT             ) name="MPI_SHORT             ";
+    else if (type==MPI_UNSIGNED_SHORT    ) name="MPI_UNSIGNED_SHORT    ";
+    else if (type==MPI_INT               ) name="MPI_INT               ";
+    else if (type==MPI_UNSIGNED          ) name="MPI_UNSIGNED          ";
+    else if (type==MPI_LONG              ) name="MPI_LONG              ";
+    else if (type==MPI_UNSIGNED_LONG     ) name="MPI_UNSIGNED_LONG     ";
+    else if (type==MPI_FLOAT             ) name="MPI_FLOAT             ";
+    else if (type==MPI_DOUBLE            ) name="MPI_DOUBLE            ";
+    else if (type==MPI_LONG_DOUBLE       ) name="MPI_LONG_DOUBLE       ";
+    else if (type==MPI_LONG_LONG_INT     ) name="MPI_LONG_LONG_INT     ";
+    else if (type==MPI_LONG_LONG         ) name="MPI_LONG_LONG         ";
+    else if (type==MPI_UNSIGNED_LONG_LONG) name="MPI_UNSIGNED_LONG_LONG";
+    else                                   name="NOT_PREDEFINED_TYPE   ";
+    printf("type = %s\n", name);
+    return;
+}
 
 int main(int argc, char* argv[])
 {
@@ -26,10 +54,16 @@ int main(int argc, char* argv[])
         }
         if (intype!=outtype) {
             printf("intype (%d) and outtype (%d) do not match!\n", intype, outtype);
+            print_typename(intype);
+            print_typename(outtype);
             errors++;
         }
 
         n *= 2;
+
+        /* If bigmpi_int_max is artificially low for debugging,
+         * MPI will throw an error inside of MPIX_Type_contiguous_x. */
+        if (n>INT_MAX) break;
     }
 
     MPI_Finalize();
