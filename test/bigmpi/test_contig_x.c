e8eeec2213fdc4be48bc6b8cc680be786c91d91a
Jeff Hammond <jeff.science@gmail.com>
2014-05-06 22:40:43 -0500

    run test on rank 0 only

--- test/bigmpi/test_contig_x.c
+++ test/bigmpi/test_contig_x.c
@@ -36,37 +36,44 @@ int main(int argc, char* argv[])
 
     MPI_Init(&argc, &argv);
 
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     MPI_Count n = 1;
     int errors = 0;
     for (int i=0; i<e; i++) {
-        printf("Building and decoding a BigMPI type for count = %zu \n", (size_t)n);
+        if (rank==0) {
+            printf("Building and decoding a BigMPI type for count = %zu \n", (size_t)n);
 
-        MPI_Datatype intype = MPI_CHAR, bigtype;
-        MPIX_Type_contiguous_x(n, intype, &bigtype);
+            MPI_Datatype intype = MPI_CHAR, bigtype;
+            MPIX_Type_contiguous_x(n, intype, &bigtype);
 
-        MPI_Count nout;
-        MPI_Datatype outtype;
-        BigMPI_Decode_contiguous_x(bigtype, &nout, &outtype);
+            MPI_Count nout;
+            MPI_Datatype outtype;
+            BigMPI_Decode_contiguous_x(bigtype, &nout, &outtype);
 
-        if (n!=nout) {
-            printf("in (%zu) and out (%zu) counts do not match!\n", (size_t)n, (size_t)nout);
-            errors++;
-        }
-        if (intype!=outtype) {
-            printf("intype (%d) and outtype (%d) do not match!\n", intype, outtype);
-            print_typename(intype);
-            print_typename(outtype);
-            errors++;
-        }
+            if (n!=nout) {
+                printf("in (%zu) and out (%zu) counts do not match!\n", (size_t)n, (size_t)nout);
+                errors++;
+            }
+            if (intype!=outtype) {
+                printf("intype (%d) and outtype (%d) do not match!\n", intype, outtype);
+                print_typename(intype);
+                print_typename(outtype);
+                errors++;
+            }
+        } /* rank==0 */
 
         n *= 2;
 
         /* If bigmpi_int_max is artificially low for debugging,
          * MPI will throw an error inside of MPIX_Type_contiguous_x. */
-        if (n>INT_MAX) break;
+        if (n>INT_MAX) {
+            MPI_Finalize();
+            return errors;
+        }
     }
-
     MPI_Finalize();
-
     return errors;
 }
