f4ccb08f780b6d88dc1520d18dc2acab73857b35
Jeff Hammond <jeff.science@gmail.com>
2014-05-23 12:53:49 -0500

    add function documentation

--- src/bigmpi_comms.c
+++ src/bigmpi_comms.c
@@ -28,3 +28,33 @@ int BigMPI_Finalize_comms(void)
 
     return MPI_SUCCESS;
 }
+
+/*
+ * Synopsis
+ *
+ * int BigMPI_Create_graph_comm(MPI_Comm old_comm, int root, MPI_Comm * graph_comm)
+ *
+ *  Input Parameter
+ *
+ *   old_comm           MPI communicator from which to create a graph comm
+ *   root               integer id of root.  if -1, create fully connected graph,
+ *                      which is appropriate for the all___ collectives.
+ *
+ * Output Parameters
+ *
+ *   graph_comm         MPI topology communicator associated with input communicator
+ *   rc                 returns the rc from the MPI graph comm create function.
+ *
+ */
+int BigMPI_Create_graph_comm(MPI_Comm old_comm, int root, MPI_Comm * graph_comm)
+{
+    int rank, size;
+    MPI_Comm_rank(old_comm, &rank);
+    MPI_Comm_size(old_comm, &size);
+
+
+
+
+
+    return MPI_SUCCESS;
+}
