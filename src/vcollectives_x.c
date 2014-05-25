11e0d5d932486bb29a05fc6e0d45350e4dca0b3c
Jeff Hammond <jeff.science@gmail.com>
2014-05-24 21:16:41 -0500

    use NB in scatterv

--- src/vcollectives_x.c
+++ src/vcollectives_x.c
@@ -43,7 +43,7 @@ int MPIX_Gatherv_x(const void *sendbuf, MPI_Count sendcount, MPI_Datatype sendty
 }
 
 int MPIX_Scatterv_x(const void *sendbuf, const MPI_Count sendcounts[], const MPI_Aint adispls[], MPI_Datatype sendtype,
-                   void *recvbuf, MPI_Count recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm)
+                    void *recvbuf, MPI_Count recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm)
 {
     int rc = MPI_SUCCESS;
 
@@ -61,30 +61,20 @@ int MPIX_Scatterv_x(const void *sendbuf, const MPI_Count sendcounts[], const MPI
 
 #ifndef BIGMPI_VCOLLS_P2P
 #else // BIGMPI_VCOLLS_P2P
-    /* There is no easy way to implement large-count using MPI_Gatherv because displs is an int. */
-
-    /* Do the local comm first to avoid deadlock. */
+    /* There is no easy way to implement large-count using MPI_Scatterv because displs is an int. */
     if (root) {
-        MPI_Aint lb /* unused */, extent;
-        MPI_Type_get_extent(recvtype, &lb, &extent);
-        MPIX_Sendrecv_x(sendbuf, sendcounts[root], sendtype, root, root /* tag */,
-                        recvbuf+adispls[root]*extent, recvcount, recvtype, root, root /* tag */,
-                        comm, MPI_STATUS_IGNORE);
-    }
-
-    /* Do the nonlocal comms... */
-    if (root) {
+        MPI_Request * reqs = malloc(size*sizeof(MPI_Request)); assert(reqs!=NULL);
         for (int i=0; i<size; i++) {
-            if (i!=root) {
-                /* TODO: Use nonblocking and waitall here instead. */
                 MPI_Aint lb /* unused */, extent;
                 MPI_Type_get_extent(sendtype, &lb, &extent);
-                MPIX_Send_x(sendbuf+adispls[i]*extent, sendcounts[i], sendtype,
-                            i /* source */, i /* tag */, comm);
-            }
+                MPIX_Isend_x(sendbuf+adispls[i]*extent, sendcounts[i], sendtype,
+                             i /* source */, i /* tag */, comm, &reqs[i]);
         }
-    } else {
-        MPIX_Recv_x(recvbuf, recvcount, recvtype, root, rank /* tag */, comm, MPI_STATUS_IGNORE);
+    }
+    MPIX_Recv_x(recvbuf, recvcount, recvtype, root, rank /* tag */, comm, MPI_STATUS_IGNORE);
+    if (root) {
+        MPI_Waitall(size, reqs, MPI_STATUSES_IGNORE);
+        free(reqs);
     }
 #endif // BIGMPI_VCOLLS_P2P
     return rc;
