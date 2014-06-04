cc19b2c2cf24ddbeaf2ab1a9fb6a89c8a390605d
Jeff Hammond <jeff.science@gmail.com>
2014-06-04 18:42:13 -0500

    fixed some of the autotools issues

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -2,48 +2,49 @@
 # Copyright (C) 2014. See LICENSE in top-level directory.
 #
 
-check_PROGRAMS += tests/test_bcast_x \
-		  tests/test_allreduce_x \
-		  tests/test_alltoall_x \
-		  tests/test_assert_x \
-		  tests/test_bcast_x \
-		  tests/test_contig_x \
-		  tests/test_gather_x \
-		  tests/test_isend_irecv_x \
-		  tests/test_reduce_x \
-		  tests/test_rma_x \
-		  tests/test_rma2_x \
-		  tests/test_scatter_x \
-		  tests/test_send_recv_x \
-		  tests/test_sendrecv_x
+check_PROGRAMS += test/test_bcast_x \
+		  test/test_allreduce_x \
+		  test/test_alltoall_x \
+		  test/test_assert_x \
+		  test/test_bcast_x \
+		  test/test_contig_x \
+		  test/test_gather_x \
+		  test/test_isend_irecv_x \
+		  test/test_reduce_x \
+		  test/test_rma_x \
+		  test/test_rma2_x \
+		  test/test_scatter_x \
+		  test/test_send_recv_x \
+		  test/test_sendrecv_x
 
-TESTS          += tests/test_bcast_x         \
-		  tests/test_allreduce_x \
-		  tests/test_alltoall_x \
-		  tests/test_assert_x \
-		  tests/test_bcast_x \
-		  tests/test_contig_x \
-		  tests/test_gather_x \
-		  tests/test_isend_irecv_x \
-		  tests/test_reduce_x \
-		  tests/test_rma_x \
-		  tests/test_rma2_x \
-		  tests/test_scatter_x \
-		  tests/test_send_recv_x \
-		  tests/test_sendrecv_x
+test          = test/test_bcast_x \
+		test/test_allreduce_x \
+		test/test_alltoall_x \
+		test/test_assert_x \
+		test/test_bcast_x \
+		test/test_contig_x \
+		test/test_gather_x \
+		test/test_isend_irecv_x \
+		test/test_reduce_x \
+		test/test_rma_x \
+		test/test_rma2_x \
+		test/test_scatter_x \
+		test/test_send_recv_x \
+		test/test_sendrecv_x
 
-XFAIL_TESTS    += tests/test_assert_x
+XFAIL_test    = test/test_assert_x
 
-tests_test_bcast_x_LDADD = libbigmpi.la
-tests_test_allreduce_x_LDADD = libbigmpi.la
-tests_test_alltoall_x_LDADD = libbigmpi.la
-tests_test_assert_x_LDADD = libbigmpi.la
-tests_test_contig_x_LDADD = libbigmpi.la
-tests_test_gather_x_LDADD = libbigmpi.la
-tests_test_isend_irecv_x_LDADD = libbigmpi.la
-tests_test_reduce_x_LDADD = libbigmpi.la
-tests_test_rma_x_LDADD = libbigmpi.la
-tests_test_rma2_x_LDADD = libbigmpi.la
-tests_test_scatter_x_LDADD = libbigmpi.la
-tests_test_send_recv_x_LDADD = libbigmpi.la
-tests_test_sendrecv_x_LDADD = libbigmpi.la
+test_test_bcast_x_LDADD = libbigmpi.la
+test_test_bcast_x_SOURCES = test/verify_buffer.c
+test_test_allreduce_x_LDADD = libbigmpi.la
+test_test_alltoall_x_LDADD = libbigmpi.la
+test_test_assert_x_LDADD = libbigmpi.la
+test_test_contig_x_LDADD = libbigmpi.la
+test_test_gather_x_LDADD = libbigmpi.la
+test_test_isend_irecv_x_LDADD = libbigmpi.la
+test_test_reduce_x_LDADD = libbigmpi.la
+test_test_rma_x_LDADD = libbigmpi.la
+test_test_rma2_x_LDADD = libbigmpi.la
+test_test_scatter_x_LDADD = libbigmpi.la
+test_test_send_recv_x_LDADD = libbigmpi.la
+test_test_sendrecv_x_LDADD = libbigmpi.la
