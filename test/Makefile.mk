cf09a0a727920a503d88483ba8a05fa04892098a
Jeff Hammond <jeff.science@gmail.com>
2014-06-13 08:37:13 -0500

    add new tests to autotools; remove bsend tests

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -16,6 +16,10 @@ check_PROGRAMS += test/test_bcast_x \
 		  test/test_scatter_x \
 		  test/test_send_recv_x \
 		  test/test_sendrecv_x \
+		  test/test_irsend_irecv_x \
+		  test/test_issend_irecv_x \
+		  test/test_rsend_recv_x \
+		  test/test_ssend_recv_x \
 		  # end
 
 TESTS        += test/test_bcast_x \
@@ -32,6 +36,10 @@ TESTS        += test/test_bcast_x \
 		test/test_scatter_x \
 		test/test_send_recv_x \
 		test/test_sendrecv_x \
+		test/test_irsend_irecv_x \
+		test/test_issend_irecv_x \
+		test/test_rsend_recv_x \
+		test/test_ssend_recv_x \
 		# end
 
 XFAIL_TESTS   += test/test_assert_x \
@@ -50,6 +58,10 @@ test_test_rma2_x_LDADD = libbigmpi.la
 test_test_scatter_x_LDADD = libbigmpi.la
 test_test_send_recv_x_LDADD = libbigmpi.la
 test_test_sendrecv_x_LDADD = libbigmpi.la
+test_test_irsend_irecv_x_LDADD = libbigmpi.la
+test_test_issend_irecv_x_LDADD = libbigmpi.la
+test_test_rsend_recv_x_LDADD = libbigmpi.la
+test_test_ssend_recv_x_LDADD = libbigmpi.la
 
 # These are not necessary
 #include test/devel/Makefile.mk
