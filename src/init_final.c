a35ff253fc0aa7111fc7cb198e057d3a3bc60dcc
Jeff Hammond <jeff.science@gmail.com>
2015-03-14 22:26:16 -0700

    allow user to init MPI separately

--- src/init_final.c
+++ src/init_final.c
@@ -2,14 +2,28 @@
 
 MPI_Datatype MPIX_GCC_FLOAT128;
 
+/* FIXME with something thread-safe */
+static int bigmpi_initialized_mpi;
+
 int MPIX_Init_thread(int * argc, char *** argv, int requested, int * provided)
 {
-    int rc = MPI_Init_thread(argc, argv, requested, provided);
+    int is_init;
+    MPI_Initialized(&is_init);
+
+    if (is_init) {
+        MPI_Init_thread(argc, argv, requested, provided);
+        bigmpi_initialized_mpi = 1;
+    } else {
+        int thread_level;
+        MPI_Query_thread(&thread_level);
+        *provided = thread_level;
+        bigmpi_initialized_mpi = 0;
+    }
 
     MPI_Type_contiguous(sizeof(__float128), MPI_BYTE, &MPIX_GCC_FLOAT128);
     MPI_Type_commit(&MPIX_GCC_FLOAT128);
 
-    return rc;
+    return MPI_SUCCESS;
 }
 
 int MPIX_Init(int * argc, char *** argv)
@@ -21,7 +35,12 @@ int MPIX_Init(int * argc, char *** argv)
 int MPIX_Finalize(void)
 {
     MPI_Type_free(&MPIX_GCC_FLOAT128);
-    return MPI_Finalize();    
+
+    if (bigmpi_initialized_mpi) {
+        MPI_Finalize();
+    }
+
+    return MPI_SUCCESS;
 }
 
 
