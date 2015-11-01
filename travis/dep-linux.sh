a8c8a2ecf7ff331c4f51b55f6462548e141e306c
Jeff Hammond <jeff.science@gmail.com>
2015-10-31 21:03:22 -0700

    Update dep-linux.sh
    add cmake since it will be tested later (because Andreas hates himself and uses CMake instead of Autotools)

--- travis/dep-linux.sh
+++ travis/dep-linux.sh
@@ -13,10 +13,10 @@ case "$os" in
         brew update
         case "$MPI_IMPL" in
             mpich|mpich3)
-                brew install mpich
+                brew install cmake mpich
                 ;; 
             openmpi)
-                brew install openmpi
+                brew install cmake openmpi
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
@@ -29,19 +29,19 @@ case "$os" in
         sudo apt-get update -q
         case "$MPI_IMPL" in
             mpich1)
-                sudo apt-get install -q gfortran mpich-shmem-bin libmpich-shmem1.0-dev
+                sudo apt-get install -q cmake gfortran mpich-shmem-bin libmpich-shmem1.0-dev
                 ;;
             mpich2)
-                sudo apt-get install -q gfortran mpich2 libmpich2-3 libmpich2-dev
+                sudo apt-get install -q cmake gfortran mpich2 libmpich2-3 libmpich2-dev
                 ;;
             mpich|mpich3)
-                sudo apt-get install -q gfortran libcr0 default-jdk
+                sudo apt-get install -q cmake gfortran libcr0 default-jdk
                 wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.2b3/mpich_3.2b3-1ubuntu_amd64.deb
                 sudo dpkg -i ./mpich_3.1-1ubuntu_amd64.deb
                 # rm -f ./mpich_3.1-1ubuntu_amd64.deb
                 ;;
             openmpi)
-                sudo apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev
+                sudo apt-get install -q cmake gfortran openmpi-bin openmpi-common libopenmpi-dev
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
