e743abc8d12105d0ae01578aa331b8e59c7b4c6b
Jeff Hammond <jeff.science@gmail.com>
2015-10-31 17:17:41 -0700

    Create build-linux.sh
    adapted from https://github.com/m-a-d-n-e-s-s/madness/blob/master/ci/build-linux.sh

---
+++ travis/build-linux.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# Exit on error
+set -ev
+
+# Environment variables
+export CFLAGS="-std=c99"
+export MPICH_CC=$CC
+export MPICC=/usr/bin/mpicc.mpich2
+
+# Configure and build MADNESS
+./autogen.sh 
+./configure \
+    --enable-g \
+    --disable-static \
+    LIBS="-lm -lpthread"
+
+if [ "$RUN_TEST" = "buildonly" ]; then
+    # Build all libraries, examples, and applications
+    make -j2 all
+else
+    # Run unit tests
+    make check
+fi
