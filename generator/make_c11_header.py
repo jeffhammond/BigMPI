5e3dbcd1ee78c29d7c6f1106de651a4092685703
Jeff Hammond <jeff.science@gmail.com>
2015-12-09 12:40:49 -0800

    add collectives

--- generator/make_c11_header.py
+++ generator/make_c11_header.py
@@ -48,6 +48,7 @@ def make_mpi_c11_decl3(f,name,args):
            +'                 (default,default,default):         MPI_' + name +  '_x)' + args + '\n\n')
 
 mpi_interfaces = [
+                  # P2P
                   [1,'Send',             '(buf,count,type,dst,tag,comm)'],
                   [1,'Ssend',            '(buf,count,type,dst,tag,comm)'],
                   [1,'Rsend',            '(buf,count,type,dst,tag,comm)'],
@@ -63,14 +64,33 @@ mpi_interfaces = [
                   [2,'Sendrecv',         '(sbuf,scount,stype,dst,stag,rbuf,rcount,rtype,src,rtag,comm,stat)'],
                   [2,'Sendrecv_replace', '(buf,scount,stype,dst,stag,rcount,rtype,src,rtag,comm,stat)'],
                   [1,'Buffer_attach',    '(buf,size)'],
-                  [2,'Put',              '(buf,scount,stype,dst,disp,rcount,rtype,win)']
-                  [2,'Get',              '(buf,scount,stype,dst,disp,rcount,rtype,win)']
-                  [2,'Accumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win)']
-                  [3,'Get_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win)']
-                  [2,'Rput',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)']
-                  [2,'Rget',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)']
-                  [2,'Raccumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win,req)']
-                  [3,'Rget_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win,req)']
+                  # RMA
+                  [2,'Put',              '(buf,scount,stype,dst,disp,rcount,rtype,win)'],
+                  [2,'Get',              '(buf,scount,stype,dst,disp,rcount,rtype,win)'],
+                  [2,'Accumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win)'],
+                  [3,'Get_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win)'],
+                  [2,'Rput',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)'],
+                  [2,'Rget',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)'],
+                  [2,'Raccumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win,req)'],
+                  [3,'Rget_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win,req)'],
+                  # COLL
+                  [1,'Bcast',                 '(buf,count,type,root,comm)'],
+                  [1,'Ibcast',                '(buf,count,type,root,comm,req)'],
+                  [2,'Gather',                '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm)'],
+                  [2,'Igather',               '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm,req)'],
+                  [2,'Allgather',             '(sbuf,scount,stype,rbuf,rcount,rtype,comm)'],
+                  [2,'Iallgather',            '(sbuf,scount,stype,rbuf,rcount,rtype,comm,req)'],
+                  [2,'Scatter',               '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm)'],
+                  [2,'Iscatter',              '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm,req)'],
+                  [2,'Alltoall',              '(sbuf,scount,stype,rbuf,rcount,rtype,comm)'],
+                  [2,'Ialltoall',             '(sbuf,scount,stype,rbuf,rcount,rtype,comm,req)'],
+                  [1,'Reduce',                '(sbuf,rbuf,count,type,op,root,comm)'],
+                  [1,'Ireduce',               '(sbuf,rbuf,count,type,op,root,comm,req)'],
+                  [1,'Allreduce',             '(sbuf,rbuf,count,type,op,comm)'],
+                  [1,'Iallreduce',            '(sbuf,rbuf,count,type,op,comm,req)'],
+                  [1,'Reduce_scatter_block',  '(sbuf,rbuf,count,type,op,comm)'],
+                  [1,'Ireduce_scatter_block', '(sbuf,rbuf,count,type,op,comm,req)'],
+
                  ]
 
 f = open('test.h','w')
