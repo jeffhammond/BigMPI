e339252c888991e50baa9ed5abecf9bb7bad4841
Jeff Hammond <jeff.science@gmail.com>
2015-12-09 11:34:45 -0800

    add skeleton C11 header

---
+++ src/mpi_c11.h
@@ -0,0 +1,14 @@
+#ifndef MPI_C11_H_INCLUDED
+#define MPI_C11_H_INCLUDED
+
+//#include "bigmpi.h"
+
+#if !defined(__cplusplus) && defined(__STDC_VERSION__) && (__STDC_VERSION__ == 201112L)
+
+
+
+
+
+#endif // C11 supported
+
+#endif // MPI_C11_H_INCLUDED
