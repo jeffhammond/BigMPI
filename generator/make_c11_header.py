ff48195225770d520020b47d6dbca9e822b68099
Jeff Hammond <jeff.science@gmail.com>
2015-12-09 14:06:03 -0800

    add a bunch of vcoll

--- generator/make_c11_header.py
+++ generator/make_c11_header.py
@@ -15,7 +15,7 @@ def make_post(f):
     f.write('#endif // C11 supported\n\n'
            +'#endif // MPI_C11_H_INCLUDED\n')
 
-def make_mpi_c11_decl(f,name,args):
+def make_mpi_c11_decl1s(f,name,args):
     f.write('#define MPI_' + name + args + '    \\\n'
            +'        _Generic((count)                          \\\n'
            +'                 short:               MPI_' + name +  '    \\\n'
@@ -23,7 +23,13 @@ def make_mpi_c11_decl(f,name,args):
            +'                 MPI_Count:           MPI_' + name +  '_x, \\\n'
            +'                 default:             MPI_' + name +  '_x)' + args + '\n\n')
 
-def make_mpi_c11_decl2(f,name,args):
+def make_mpi_c11_decl1v(f,name,args):
+    f.write('#define MPI_' + name + args + '    \\\n'
+           +'        _Generic((counts)                          \\\n'
+           +'                 int*:                 MPI_' + name +  '    \\\n'
+           +'                 MPI_Count*:           MPI_' + name +  '_x)' + args + '\n\n')
+
+def make_mpi_c11_decl2s(f,name,args):
     f.write('#define MPI_' + name + args + '    \\\n'
            +'        _Generic((scount,rcount)                           \\\n'
            +'                 (short,short):               MPI_' + name +  '    \\\n'
@@ -33,7 +39,25 @@ def make_mpi_c11_decl2(f,name,args):
            +'                 (MPI_Count,MPI_Count):       MPI_' + name +  '_x, \\\n'
            +'                 (default,default):           MPI_' + name +  '_x)' + args + '\n\n')
 
-def make_mpi_c11_decl3(f,name,args):
+def make_mpi_c11_decl1s1v(f,name,args):
+    f.write('#define MPI_' + name + args + '    \\\n'
+           +'        _Generic((count,vcount)                           \\\n'
+           +'                 (int,int*):                   MPI_' + name +  '    \\\n'
+           +'                 (MPI_Count,MPI_Count*):       MPI_' + name +  '_x)' + args + '\n\n')
+
+def make_mpi_c11_decl2v(f,name,args):
+    f.write('#define MPI_' + name + args + '    \\\n'
+           +'        _Generic((scounts,rcounts)                           \\\n'
+           +'                 (int*,int*):                   MPI_' + name +  '    \\\n'
+           +'                 (MPI_Count*,MPI_Count*):       MPI_' + name +  '_x)' + args + '\n\n')
+
+def make_mpi_c11_decl1s2v(f,name,args):
+    f.write('#define MPI_' + name + args + '    \\\n'
+           +'        _Generic((count,counts,displs)                           \\\n'
+           +'                 (int,int*,int*):                   MPI_' + name +  '    \\\n'
+           +'                 (MPI_Count,MPI_Count*,MPI_Aint*):  MPI_' + name +  '_x)' + args + '\n\n')
+
+def make_mpi_c11_decl3s(f,name,args):
     f.write('#define MPI_' + name + args + '    \\\n'
            +'        _Generic((ocount,rcount,tcount)                      \\\n'
            +'                 (short,short,short):               MPI_' + name +  '    \\\n'
@@ -47,58 +71,78 @@ def make_mpi_c11_decl3(f,name,args):
            +'                 (MPI_Count,MPI_Count,MPI_Count):   MPI_' + name +  '_x, \\\n'
            +'                 (default,default,default):         MPI_' + name +  '_x)' + args + '\n\n')
 
+def make_mpi_c11_decl4v(f,name,args):
+    f.write('#define MPI_' + name + args + '    \\\n'
+           +'        _Generic((scounts,sdispls,rcounts,rdispls)                           \\\n'
+           +'                 (int*,int*,int*,int*):                        MPI_' + name +  '    \\\n'
+           +'                 (MPI_Count*,MPI_Aint*,MPI_Count*,MPI_Aint*):  MPI_' + name +  '_x)' + args + '\n\n')
+
 mpi_interfaces = [
                   # P2P
-                  [1,'Send',             '(buf,count,type,dst,tag,comm)'],
-                  [1,'Ssend',            '(buf,count,type,dst,tag,comm)'],
-                  [1,'Rsend',            '(buf,count,type,dst,tag,comm)'],
-                  [1,'Bsend',            '(buf,count,type,dst,tag,comm)'],
-                  [1,'Isend',            '(buf,count,type,dst,tag,comm,req)'],
-                  [1,'Issend',           '(buf,count,type,dst,tag,comm,req)'],
-                  [1,'Irsend',           '(buf,count,type,dst,tag,comm,req)'],
-                  [1,'Ibsend',           '(buf,count,type,dst,tag,comm,req)'],
-                  [1,'Recv',             '(buf,count,type,src,tag,comm,stat)'],
-                  [1,'Irecv',            '(buf,count,type,src,tag,comm,req)'],
-                  [1,'Mrecv',            '(buf,count,type,msg,stat)'],
-                  [1,'Imrecv',           '(buf,count,type,msg,req)'],
-                  [2,'Sendrecv',         '(sbuf,scount,stype,dst,stag,rbuf,rcount,rtype,src,rtag,comm,stat)'],
-                  [2,'Sendrecv_replace', '(buf,scount,stype,dst,stag,rcount,rtype,src,rtag,comm,stat)'],
-                  [1,'Buffer_attach',    '(buf,size)'],
+                  ['1s','Send',             '(buf,count,type,dst,tag,comm)'],
+                  ['1s','Ssend',            '(buf,count,type,dst,tag,comm)'],
+                  ['1s','Rsend',            '(buf,count,type,dst,tag,comm)'],
+                  ['1s','Bsend',            '(buf,count,type,dst,tag,comm)'],
+                  ['1s','Isend',            '(buf,count,type,dst,tag,comm,req)'],
+                  ['1s','Issend',           '(buf,count,type,dst,tag,comm,req)'],
+                  ['1s','Irsend',           '(buf,count,type,dst,tag,comm,req)'],
+                  ['1s','Ibsend',           '(buf,count,type,dst,tag,comm,req)'],
+                  ['1s','Recv',             '(buf,count,type,src,tag,comm,stat)'],
+                  ['1s','Irecv',            '(buf,count,type,src,tag,comm,req)'],
+                  ['1s','Mrecv',            '(buf,count,type,msg,stat)'],
+                  ['1s','Imrecv',           '(buf,count,type,msg,req)'],
+                  ['2s','Sendrecv',         '(sbuf,scount,stype,dst,stag,rbuf,rcount,rtype,src,rtag,comm,stat)'],
+                  ['2s','Sendrecv_replace', '(buf,scount,stype,dst,stag,rcount,rtype,src,rtag,comm,stat)'],
+                  ['1s','Buffer_attach',    '(buf,size)'],
                   # RMA
-                  [2,'Put',              '(buf,scount,stype,dst,disp,rcount,rtype,win)'],
-                  [2,'Get',              '(buf,scount,stype,dst,disp,rcount,rtype,win)'],
-                  [2,'Accumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win)'],
-                  [3,'Get_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win)'],
-                  [2,'Rput',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)'],
-                  [2,'Rget',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)'],
-                  [2,'Raccumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win,req)'],
+                  ['2s','Put',              '(buf,scount,stype,dst,disp,rcount,rtype,win)'],
+                  ['2s','Get',              '(buf,scount,stype,dst,disp,rcount,rtype,win)'],
+                  ['2s','Accumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win)'],
+                  ['3s','Get_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win)'],
+                  ['2s','Rput',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)'],
+                  ['2s','Rget',              '(buf,scount,stype,dst,disp,rcount,rtype,win,req)'],
+                  ['2s','Raccumulate',       '(buf,scount,stype,dst,disp,rcount,rtype,op,win,req)'],
                   [3,'Rget_accumulate',   '(obuf,ocount,otype,rbuf,rcount,rtype,dst,disp,tcount,ttype,op,win,req)'],
                   # COLL
-                  [1,'Bcast',                 '(buf,count,type,root,comm)'],
-                  [1,'Ibcast',                '(buf,count,type,root,comm,req)'],
-                  [2,'Gather',                '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm)'],
-                  [2,'Igather',               '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm,req)'],
-                  [2,'Allgather',             '(sbuf,scount,stype,rbuf,rcount,rtype,comm)'],
-                  [2,'Iallgather',            '(sbuf,scount,stype,rbuf,rcount,rtype,comm,req)'],
-                  [2,'Scatter',               '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm)'],
-                  [2,'Iscatter',              '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm,req)'],
-                  [2,'Alltoall',              '(sbuf,scount,stype,rbuf,rcount,rtype,comm)'],
-                  [2,'Ialltoall',             '(sbuf,scount,stype,rbuf,rcount,rtype,comm,req)'],
-                  [1,'Reduce',                '(sbuf,rbuf,count,type,op,root,comm)'],
-                  [1,'Ireduce',               '(sbuf,rbuf,count,type,op,root,comm,req)'],
-                  [1,'Allreduce',             '(sbuf,rbuf,count,type,op,comm)'],
-                  [1,'Iallreduce',            '(sbuf,rbuf,count,type,op,comm,req)'],
-                  [1,'Reduce_scatter_block',  '(sbuf,rbuf,count,type,op,comm)'],
-                  [1,'Ireduce_scatter_block', '(sbuf,rbuf,count,type,op,comm,req)'],
-
+                  ['1s','Bcast',                 '(buf,count,type,root,comm)'],
+                  ['1s','Ibcast',                '(buf,count,type,root,comm,req)'],
+                  ['2s','Gather',                '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm)'],
+                  ['2s','Igather',               '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm,req)'],
+                  ['2s','Allgather',             '(sbuf,scount,stype,rbuf,rcount,rtype,comm)'],
+                  ['2s','Iallgather',            '(sbuf,scount,stype,rbuf,rcount,rtype,comm,req)'],
+                  ['2s','Scatter',               '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm)'],
+                  ['2s','Iscatter',              '(sbuf,scount,stype,rbuf,rcount,rtype,root,comm,req)'],
+                  ['2s','Alltoall',              '(sbuf,scount,stype,rbuf,rcount,rtype,comm)'],
+                  ['2s','Ialltoall',             '(sbuf,scount,stype,rbuf,rcount,rtype,comm,req)'],
+                  ['1s','Reduce',                '(sbuf,rbuf,count,type,op,root,comm)'],
+                  ['1s','Ireduce',               '(sbuf,rbuf,count,type,op,root,comm,req)'],
+                  ['1s','Allreduce',             '(sbuf,rbuf,count,type,op,comm)'],
+                  ['1s','Iallreduce',            '(sbuf,rbuf,count,type,op,comm,req)'],
+                  ['1s','Reduce_scatter_block',  '(sbuf,rbuf,count,type,op,comm)'],
+                  ['1s','Ireduce_scatter_block', '(sbuf,rbuf,count,type,op,comm,req)'],
+                  ['1v','Reduce_scatter',        '(sbuf,rbuf,counts,type,op,comm)'],
+                  ['1v','Ireduce_scatter',       '(sbuf,rbuf,counts,type,op,comm,req)'],
+                  ['1s2v','Scatterv',            '(sbuf,counts,displs,stype,rbuf,count,rtype,root,comm)'],
+                  ['1s2v','Iscatterv',           '(sbuf,counts,displs,stype,rbuf,count,rtype,root,comm,req)'],
+                  ['1s2v','Gatherv',             '(sbuf,count,stype,rbuf,counts,displs,rtype,root,comm)'],
+                  ['1s2v','Igatherv',            '(sbuf,count,stype,rbuf,counts,displs,rtype,root,comm,req)'],
+                  ['1s2v','Allgatherv',          '(sbuf,count,stype,rbuf,counts,displs,rtype,comm)'],
+                  ['1s2v','Iallgatherv',         '(sbuf,count,stype,rbuf,counts,displs,rtype,comm,req)'],
+                  ['4v','Alltoallv',             '(sbuf,scounts,sdispls,stype,rbuf,rcounts,rdispls,rtype,comm)'],
+                  ['4v','Ialltoallv',            '(sbuf,scounts,sdispls,stype,rbuf,rcounts,rdispls,rtype,comm,req)'],
+                  ['4v','Alltoallw',             '(sbuf,scounts,sdispls,stypes,rbuf,rcounts,rdispls,rtypes,comm)'],
+                  ['4v','Ialltoallw',            '(sbuf,scounts,sdispls,stypes,rbuf,rcounts,rdispls,rtypes,comm,req)']
                  ]
 
 f = open('test.h','w')
 make_pre(f)
 for (n,name,args) in mpi_interfaces:
-    if   n==1: make_mpi_c11_decl(f,name,args)
-    elif n==2: make_mpi_c11_decl2(f,name,args)
-    elif n==3: make_mpi_c11_decl3(f,name,args)
+    if   n=='1s':   make_mpi_c11_decl1s(f,name,args)
+    elif n=='2s':   make_mpi_c11_decl2s(f,name,args)
+    elif n=='3s':   make_mpi_c11_decl3s(f,name,args)
+    elif n=='1s1v': make_mpi_c11_decl1s1v(f,name,args)
+    elif n=='1s2v': make_mpi_c11_decl1s2v(f,name,args)
+    elif n=='4v':   make_mpi_c11_decl4v(f,name,args)
 make_post(f)
 f.close()
 
